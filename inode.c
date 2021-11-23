b440205d755abbb292f41fd9dfb561f6c4641752
Il-jun <69421625+Il-jun@users.noreply.github.com>
2021-11-23 12:20:14 +0900

    Add files via upload

--- inode.c
+++ inode.c
@@ -2,25 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-
-typedef struct inode{
-	_Bool kind;//디럭터리인지 파일인지 구분
-	time_t t;//만들어진 시간을 확인
-	int  * p;//사용되는 데이터 블록 확인
-}inode; // inode를 위한 구조체 선언
-
-typedef struct ilist{
-	inode * ptr;
-	struct ilist * next;
-}ilist;
-
-ilist il;
-
-void addilist (inode * i){
-	ilist * ptr = malloc(sizeof(ilist));
-	il.next = ptr;
-	ptr -> ptr = i;
-}
+#include "inode.h"
 
 int makeinode (inode * i, _Bool k, int num, int use){//만들고자 하는 inode, 종류, 저장될 데이터블록 시작점, 몇개 사용할 것인지
 	i -> p = calloc(use, sizeof(int)); //사용되는 데이터블럭 동적 할당
@@ -29,15 +11,5 @@ int makeinode (inode * i, _Bool k, int num, int use){//만들고자 하는 inode
 	for (int j = 0; j<use; j++){
 		i->p[j] = (num+1+j);//data블록에 순차적으로 저장
 	}
-	addilist(i);
 	return (num+use+1);//다음 저장될 곳 리턴
-}
-
-int main(){
-	int start = 0;
-	int next;
-	inode i1;
-	next = makeinode(&i1, false, start, 4);
-	printf ("kind : %s\ntime : %ld\ndata : 1-%d 2-%d 3-%d\nnext : %d\n", i1.kind ? "true" : "false", i1.t, i1.p[0], i1.p[1], i1.p[2], next);
-	free (i1.p);
 }
