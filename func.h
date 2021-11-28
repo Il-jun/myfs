925398103b4737eb0d4db4e3a9b38f8c17b7b84d
Il-jun <69421625+Il-jun@users.noreply.github.com>
2021-11-28 16:50:05 +0900

    Add files via upload

--- func.h
+++ func.h
@@ -6,10 +6,19 @@
 
 int make_fs ();
 void print_shell();
-char * scan_comm();
 void op_comm(char *);
 int make_inode (inode *, _Bool, int, int);
 void set_sblock (_Bool, int);
+
+void print_prompt ();
+void print_pwd ();
+void set_root_path ();
+void set_next_path (char *);
+void cmp_comm (char *, char *, char *);
+void scan_comm (char **, char **, char **);
+
+void show_ls ();
+
 // stack 구현 함수
 void s_reset();
 void s_push(char c);
---
+++ scan_comm.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "func.h"
+
+int main () {
+	char * comm1 = NULL, * comm2 = NULL, * comm3 = NULL;
+	scan_comm(&comm1, &comm2, &comm3);
+	printf ("%s %s %s\n", comm1, comm2, comm3);
+	cmp_comm(comm1, comm2, comm3);
+}
+
+void scan_comm (char ** comm1, char ** comm2, char ** comm3) {
+	char *temp;
+	char *p_str = NULL;
+	int total_size = 0, size = 0, comm_num = 1; 
+		
+	temp = malloc(sizeof(char));
+	while (1) {  
+		fgets(temp, 2, stdin);
+		size = strlen(temp);
+		total_size += size;
+		if (temp[0] == ' '){
+			switch (comm_num) {
+				case 1 :
+					*comm1 = p_str;
+					break;
+				case 2 :
+					*comm2 = p_str;
+					break;
+			}
+			p_str = NULL;
+			comm_num++;
+			continue;
+		}
+		if (temp[0] == '\n') {
+			*comm3 = p_str;
+			break;
+		}
+			
+		if (p_str == NULL) {  
+			p_str = (char *)malloc(total_size + 1);
+			strcpy(p_str, temp);
+		}
+		else {
+			p_str = (char *)realloc(p_str, total_size + 1);
+			strcat(p_str, temp);
+		}
+	}
+}
+
+void cmp_comm (char * comm1, char * comm2, char * comm3) {
+	if (!strcmp(comm1, "myfs"))
+		//make_fs();
+		printf("1\n");
+	if (!strcmp(comm1, "myls"))
+		//show_ls();
+	if (!strcmp(comm1, "mycat"))
+		
+	if (!strcmp(comm1, "myshowfile"))
+		
+	if (!strcmp(comm1, "mypwd"))
+		//print_pwd();
+	if (!strcmp(comm1, "mycd"))
+		
+	if (!strcmp(comm1, "mycp"))
+		
+	if (!strcmp(comm1, "mycpto"))
+		
+	if (!strcmp(comm1, "mycpfrom"))
+		
+	if (!strcmp(comm1, "mymkdir"))
+		
+	if (!strcmp(comm1, "myrmdir"))
+		
+	if (!strcmp(comm1, "myrm"))
+		
+	if (!strcmp(comm1, "mymv"))
+		
+	if (!strcmp(comm1, "mytouch"))
+		
+	if (!strcmp(comm1, "myinode"))
+		
+	if (!strcmp(comm1, "mydatablock"))
+		
+	if (!strcmp(comm1, "mystate"))
+		
+	if (!strcmp(comm1, "mytree"))
+		
+	if (!strcmp(comm1, "exit"))
+		exit(0);
+}
